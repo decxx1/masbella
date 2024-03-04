3d6e1546ee0d9586f740ce38ec58a6bdde828c90
Damian Cisternas <decxx1@gmail.com>
2024-03-04 20:16:45 -0300

    astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  site: 'https://masbellamedical.com/',
+  build: {
+    // Ejemplo: Genera `page.html` en lugar de `page/index.html` durante la compilación.
+    format: 'file'
+  },
   integrations: [react()]
 });
