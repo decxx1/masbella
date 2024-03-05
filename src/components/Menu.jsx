2c167a8e92c8a600ed806a3a95fe10cbe7b3a796
Damian Cisternas <decxx1@gmail.com>
2024-03-05 15:52:58 -0300

    build franchise page

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,18 +1,38 @@
+import { useEffect, useState } from "react";
 
-export default function Menu(props) {
-    return (
-        <div className="header__menu__option">
-          <nav className="header__menu">
-            <ul>
-              <li className={props.path === '/' ? 'active' : ''}><a href='./'>Inicio</a></li>
-              <li className={props.path === '/servicios' ? 'active' : ''}><a href='./servicios'>Servicios</a></li>
-              <li className={props.path === '/about' ? 'active' : ''}><a href='./about'>Nosotros</a></li>
-              <li className={props.path === '/franquicias' ? 'active' : ''}><a href='./franquicias'>Franquicias</a></li>
-            </ul>
-          </nav>
-          <div className="header__btn">
-            <a href="./contacto" className={props.path === '/contacto' ? 'contact-select primary-btn' : 'primary-btn'}>Contacto</a>
-          </div>
-        </div>
-    )
+export default function Menu() {
+  const [path, setPath] = useState(null);
+  useEffect(() => {
+    setPath(window.location.pathname);
+  }, []);
+  return (
+    <div className="header__menu__option">
+      <nav className="header__menu">
+        <ul>
+          <li className={path === "/" ? "active" : ""}>
+            <a href="./">Inicio</a>
+          </li>
+          <li className={path === "/servicios" ? "active" : ""}>
+            <a href="./servicios">Servicios</a>
+          </li>
+          <li className={path === "/about" ? "active" : ""}>
+            <a href="./about">Nosotros</a>
+          </li>
+          <li className={path === "/franquicias" ? "active" : ""}>
+            <a href="./franquicias">Franquicias</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="header__btn">
+        <a
+          href="./contacto"
+          className={
+            path === "/contacto" ? "contact-select primary-btn" : "primary-btn"
+          }
+        >
+          Contacto
+        </a>
+      </div>
+    </div>
+  );
 }
