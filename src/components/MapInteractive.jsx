d3311d07057a4e7ac2a8112c59d689fbce75a897
Damian Cisternas <decxx1@gmail.com>
2024-12-16 18:25:11 -0300

    new map interactive

--- src/components/MapInteractive.jsx
+++ src/components/MapInteractive.jsx
@@ -4,11 +4,11 @@ export function LineMdMapMarkerAltFilled(props) {
 	return (<svg className="inline text-primary-100 w-11 h-11 pb-2 group-hover:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}><mask id="lineMdMapMarkerAltFilled0"><path fill="#fff" stroke="#fff" strokeLinecap="round" strokeWidth={2} d="M12 20.5C12 20.5 6 13.5 6 9C6 5.68629 8.68629 3 12 3C15.3137 3 18 5.68629 18 9C18 13.5 12 20.5 12 20.5z"><animate fill="freeze" attributeName="d" dur="0.4s" keyTimes="0;0.7;1" values="M12 20.5C12 20.5 11 19 11 18C11 17.5 11.5 17 12 17C12.5 17 13 17.5 13 18C13 19 12 20.5 12 20.5z;M12 20.5C12 20.5 5 13 5 8C5 4.5 8 1 12 1C16 1 19 4.5 19 8C19 13 12 20.5 12 20.5z;M12 20.5C12 20.5 6 13.5 6 9C6 5.68629 8.68629 3 12 3C15.3137 3 18 5.68629 18 9C18 13.5 12 20.5 12 20.5z"></animate></path><circle cx={12} cy={9} r={2.5} fillOpacity={0}><animate fill="freeze" attributeName="fill-opacity" begin="0.3s" dur="0.15s" values="0;1"></animate></circle></mask><rect width={24} height={24} fill="currentColor" mask="url(#lineMdMapMarkerAltFilled0)"></rect></svg>);
 }
 export default function MapInteractive () {
-    const mapDefault = '/img/map/map_full.webp';
+    const mapDefault = '/img/map/map_full.svg';
     const [map, setMap] = useState(mapDefault);
     
-    const malaga = '/img/map/map_malaga_01.webp';
-    const cordoba = '/img/map/map_cordoba_01.webp';
+    const malaga = '/img/map/map_malaga_01.svg';
+    const cordoba = '/img/map/map_cordoba_01.svg';
 
     const updateMap = (city) => {
         setMap(city);
